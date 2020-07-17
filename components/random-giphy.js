f127f012c230a262813b0a64cc88ebdc93712f11
Håkon Krogh <hakonhk@gmail.com>
2020-07-17 22:42:25 +0200

    Fixed relax period giph image

--- components/random-giphy.js
+++ components/random-giphy.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-export default function RandomGiphy({ tag = "workout,fail" }) {
+export default function RandomGiphy({ tag = "workout,+fail" }) {
   const [image, setImage] = useState(null);
 
   async function getNew() {
